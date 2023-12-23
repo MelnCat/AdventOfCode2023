6bb8dfaef331e4b77f484978e00bf10308df982e
MelnCat <melncatuwu@gmail.com>
2023-12-23 00:26:42 -0800

    pain

--- day-23/index2.js
+++ day-23/index2.js
@@ -9,57 +9,81 @@ const grid = data.split("\n").map(x => x.split(""));
 const start = [0, grid[0].indexOf(".")];
 const end = [grid.length - 1, grid[grid.length - 1].indexOf(".")];
 
-let checks = [{ curr: start, walked: new Set(), len: 0, last: [null, null], previ: { curr: start, len: 0 } }];
-const ended = [];
-let n = 0;
-const intersectionMax = {};
-const grab = {};
-while (checks.length) {
-	const check = checks.shift();
-	if (check.curr[0] === end[0] && check.curr[1] === end[1]) {
-		ended.push(check);
-		continue;
-	}
+const cache = {}
+const findConnections = s => {
+	if (cache[`${s[0]},${s[1]}`]) return cache[`${s[0]},${s[1]}`];
+	const checks = [{ curr: s, walked: new Set([`${s[0]},${s[1]}`]), len: 0 }];
+	const found = []
+	while (checks.length) {
+		const check = checks.shift();
 
-	if (!(n++ % 1000)) console.log("CURR " + n++, checks.length);
-	const neighbors = [
-		[0, 1],
-		[1, 0],
-		[-1, 0],
-		[0, -1],
-	]
-		.map(x => [x[0] + check.curr[0], x[1] + check.curr[1]])
-		.filter(
+		const av = [
+			[0, 1],
+			[1, 0],
+			[-1, 0],
+			[0, -1],
+		];
+
+		const adjs = av.map(x => [check.curr[0] + x[0], check.curr[1] + x[1]]).filter(
 			x =>
 				x[0] >= 0 &&
 				x[0] < grid.length &&
 				x[1] >= 0 &&
 				x[1] < grid[0].length &&
-				grid[x[0]][x[1]] !== "#" &&
-				!(x[0] === check.last[0] && x[1] === check.last[1]) &&
-				!check.walked.has(`${x[0]},${x[1]}`)
+				grid[x[0]][x[1]] !== "#"
 		);
-			let previ = check.previ;
-	if (neighbors.length > 1) {
-		intersectionMax[`${check.curr[0]},${check.curr[1]}`] ??= [];
-		intersectionMax[`${check.curr[0]},${check.curr[1]}`].push(check.len);
-		grab[`${previ.curr[0]},${previ.curr[1]}`] ??= [];
-		const t= grab[`${previ.curr[0]},${previ.curr[1]}`].find(x => x.to === check.curr.join(","));
-		if (t) {
-			t.l = Math.max(t.l, check.len - previ.len);
-		} else
-		grab[`${previ.curr[0]},${previ.curr[1]}`].push({ l: check.len - previ.len, to: check.curr.join(",") });
-		previ = check;
+
+		if (adjs.length > 2 && check.curr !== s) {
+			found.push(check);
+			continue;
+		}
+
+		for (const next of adjs) {
+			if (check.walked.has(`${next[0]},${next[1]}`)) continue;
+			if (next[0] === end[0] && next[1] === end[1]) {
+				found.push({ curr: next, len: check.len + 1 });
+				continue;
+			};
+			checks.push({
+				curr: next,
+				walked: new Set([...check.walked, `${next[0]},${next[1]}`]),
+				len: check.len + 1,
+			});
+		}
 	}
-	for (const next of neighbors) {
-		checks.push({
-			curr: next,
-			walked: new Set([...check.walked, `${next[0]},${next[1]}`]),
-			len: check.len + 1,
-			last: check.curr,
-			previ,
+	cache[`${s[0]},${s[1]}`] = found.map(x=>({ to: x.curr, cost: x.len }));
+	return found.map(x=>({ to: x.curr, cost: x.len }));
+};
+
+const map = {};
+let list = [start];
+while (list.length) {
+	const next = list.shift();
+	if (next[0] === end[0] && next[1] === end[1]) continue;
+	const connections = findConnections(next);
+	map[`${next[0]},${next[1]}`] = connections;
+	list.push(...connections.map(x=>x.to).filter(x => !(`${x[0]},${x[1]}` in map)));
+}
+
+const paths = [{ curr: start, walked: new Set(), len: 0 }];
+let lens = []
+console.log(`done f`)
+let k = 0;
+while (paths.length) {
+	const path = paths.pop();
+	if (k++ % 1000 === 0) console.log(k, paths.length, lens.sort((a,b)=>b-a).at(0));
+	if (path.curr[0] === end[0] && path.curr[1] === end[1]) {
+		lens.push(path.len);
+		continue;
+	}
+	for (const next of map[`${path.curr[0]},${path.curr[1]}`]) {
+		if (path.walked.has(`${next.to[0]},${next.to[1]}`) || path.walked.has(`${next.to[1]},${next.to[0]}`)) continue;
+		paths.push({
+			curr: next.to,
+			walked: new Set([...path.walked, `${next.to[0]},${next.to[1]}`]),
+			len: path.len + next.cost,
 		});
 	}
 }
-console.log(grab)
-console.log(Math.max(...ended.map(x => x.len)));
+
+console.log(Math.max(...lens))
--- day-23/index25.js
+++ day-23/index25.js
@@ -9,45 +9,35 @@ const grid = data.split("\n").map(x => x.split(""));
 const start = [0, grid[0].indexOf(".")];
 const end = [grid.length - 1, grid[grid.length - 1].indexOf(".")];
 
-let checks = [{ curr: start, walked: new Set(), len: 0, last: [null, null], previ: null }];
+const checks = [{ curr: start, walked: new Set(), len: 0 }];
 const ended = [];
-let n = 0;
-const intersectionMax = {};
-const grab = {};
+
 while (checks.length) {
 	const check = checks.shift();
 	if (check.curr[0] === end[0] && check.curr[1] === end[1]) {
 		ended.push(check);
 		continue;
-	}
+	};
+	const av = [[0,1], [1,0], [-1, 0], [0, -1]];
 
-	if (!(n++ % 1000)) console.log("CURR " + n++, checks.length);
-	const neighbors = [
-		[0, 1],
-		[1, 0],
-		[-1, 0],
-		[0, -1],
-	]
-		.map(x => [x[0] + check.curr[0], x[1] + check.curr[1]])
-		.filter(
-			x =>
-				x[0] >= 0 &&
-				x[0] < grid.length &&
-				x[1] >= 0 &&
-				x[1] < grid[0].length &&
-				grid[x[0]][x[1]] !== "#" &&
-				!(x[0] === check.last[0] && x[1] === check.last[1]) &&
-				!check.walked.has(`${x[0]},${x[1]}`)
-		);
-	for (const next of neighbors) {
+	for (const adj of av) {
+		const next = [check.curr[0] + adj[0], check.curr[1] + adj[1]];
+		if (next[0] < 0 || next[0] >= grid.length) continue;
+		if (next[1] < 0 || next[1] >= grid[0].length) continue;
+		if (grid[next[0]][next[1]] === "#") continue;
+		if (check.walked.has(`${next[0]},${next[1]}`)) continue;
 		checks.push({
 			curr: next,
 			walked: new Set([...check.walked, `${next[0]},${next[1]}`]),
 			len: check.len + 1,
-			last: check.curr,
-			previ: check,
 		});
 	}
 }
-console.log(grab)
-console.log(Math.max(...ended.map(x => x.len)));
+
+const map = {};
+let list = [start];
+while (list.length) {
+	const next = list.shift();
+	if (next[0] === end[0] && next[1] === end[1]) continue;
+	
+}
